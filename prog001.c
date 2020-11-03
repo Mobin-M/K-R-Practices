d54f0c308ac0b685fde3517377e05a1d5d03a903
Mobin <mobinmohammadnia@gmail.com>
2020-11-03 19:26:34 +0100

    0311

--- prog001.c
+++ prog001.c
@@ -5,22 +5,22 @@ int main()
 {
 
 	int c;
-	char longest[200];
 	int line[200];
-	int maxlen=0;
-	int len=0;
-	// read a line, save it inside line, output the length, if higher that maxlength save it in longest
+	int len = 0;
+	//read a line save it in line[200] and count the length
 	while ((c = getchar())!= EOF)
 	{
 		if (c = '\n')
 		{
-			line[len] = '\n';
+			//line[len] = '\n';
+			//++len;
+			//line[len] = '\0';
 			for (int j = 0; j <= len; ++j)
-				printf("%d",line[j]);
+				printf("%c",line[j]);
 		}
 		else
 		{
-			line[len] = c;	
+			line[len] = c;
 			++len;
 		}
 
